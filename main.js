92aea3dcbe3f4e4813f266d250a2f34d40b2b88c
Lucas Plumpe <Lucas.plumpe@gmail.com>
2019-04-30 15:20:05 +0200

    Adding basic features to mainframe

---
+++ main.js
@@ -0,0 +1,12 @@
+<script>
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDzm94W_BXjCQVwpF3y1c54r6BXIuonhqA",
+  authDomain: "build-c992f.firebaseapp.com",
+  databaseURL: "https://build-c992f.firebaseio.com",
+  projectId: "build-c992f",
+  storageBucket: "build-c992f.appspot.com",
+  messagingSenderId: "728930746192"
+};
+firebase.initializeApp(config);
+</script>
