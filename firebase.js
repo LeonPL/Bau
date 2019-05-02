156c0ee3375bac7d7f208589ecc266f12c1e4eda
Leon Plumpe <leon.plumpe@t-online.de>
2019-05-02 10:03:48 +0200

    comment

---
+++ firebase.js
@@ -0,0 +1,15 @@
+var app_firebase = {};
+(function(){
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDzm94W_BXjCQVwpF3y1c54r6BXIuonhqA",
+  authDomain: "build-c992f.firebaseapp.com",
+  databaseURL: "https://build-c992f.firebaseio.com",
+  projectId: "build-c992f",
+  storageBucket: "build-c992f.appspot.com",
+  messagingSenderId: "728930746192"
+};
+firebase.initializeApp(config);
+
+app_firebase = firebase;
+})();
--- login.js
+++ login.js
@@ -1,78 +1,36 @@
-var database;
-var data;
-var ref;
-var uname;
-var upass;
-var usname;
-var uspass;
-var keys;
-var logi;
-
-function connect() {
-  var config = {
-  apiKey: "AIzaSyDzm94W_BXjCQVwpF3y1c54r6BXIuonhqA",
-  authDomain: "build-c992f.firebaseapp.com",
-  databaseURL: "https://build-c992f.firebaseio.com",
-  projectId: "build-c992f",
-  storageBucket: "build-c992f.appspot.com",
-  messagingSenderId: "728930746192"
-};
-firebase.initializeApp(config);
-database = firebase.database();
-
-  ref = database.ref('Konten');
-ref.on('value',gotData, errData);
-}
-function input() {
-uname= "";
-upass= "";
- uname = document.getElementById("uname").value;
- upass = document.getElementById("upass").value;
- }
-
-function save() {
-  input();
-
-   data = {
-  Username: uname,
-  Password: upass
-  };
-
-  ref = database.ref('Konten');
-  ref.push(data);
-
-   document.getElementById("uname").value="";
-   document.getElementById("upass").value="";
-
-}
-
-function errData(err) {
-  console.log('Error!');
-  console.log(err);
-}
-
-function gotData(data) {
-  console.log('hi');
-  logi = 0;
-usname = document.getElementById("uname").value;
-uspass = document.getElementById("upass").value;
-  console.log(usname,uspass);
-  var Konten = data.val();
-  keys = Object.keys(Konten);
-  console.log(keys,uname,upass);
-  for (var i = 0; i < keys.length; i++) {
-    k = keys[i];
-    if (usname ==  Konten[k].uname || uspass == Konten[k].upass) {
-      logi = 1;
+(function(){
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
     }
-  }
-  if (logi == 1) {
-    alert('Anmeldung erfolgreich');
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: 'main.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
 
-
-  }
-  if (logi == 0) {
-    alert('Anmeldung fehlgeschlagen');
-  }
-
-}
+  ],
+  // Terms of service url.
+  tosUrl: 'main.html',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+ui.start('#firebaseui-auth-container', uiConfig);
+})();
